00a299347d46cb04a3d85e5e00d9549891dfbcc4
weilok2021 <weilok629@gmail.com>
2025-10-16 18:10:21 +0800

    insert, prepend, delete, remove is implemented for dynamic array

--- dynamic_array.py
+++ dynamic_array.py
@@ -24,10 +24,10 @@ class DynamicArray:
         return self.size() == 0
 
 
-    def at(self, i):
-        if i >= self.size() or i < 0:
+    def at(self, index):
+        if index >= self.size() or index < 0:
             raise IndexError("Index out of bound")
-        return self._array[i]
+        return self._array[index]
 
 
     def push(self, item):
@@ -54,6 +54,36 @@ class DynamicArray:
         return last_elem
     
 
+    def insert(self, index, item):
+        if index > self.size() or index < 0:
+            raise IndexError("Index out of bound")
+
+        # array reach its capacity
+        if self.size() == self.capacity():
+            # resize to double the size
+            self.resize(self.capacity() * 2)
+        
+
+        for i in range(self.size(), index, -1):
+            #[1,2,3,4,5]
+            # call a.insert(0, 10)
+            # when i = 5: [1,2,3,4,5,5]
+            # when i = 4: [1,2,3,4,4,5]
+            # ...
+            # when i = 1: [1,1,2,3,4,5]
+            
+            # shift current element one index to the right
+            self._array[i] = self._array[i - 1]
+
+        # insert item into this empty position (the old elements are shifted to the right)
+        self._array[index] = item        
+        self._size += 1
+    
+
+    def prepend(self, item):
+        self.insert(0, item)
+
+
     def resize(self, new_capacity):
         double_capacity_array = (c_int * new_capacity)()  # allocate new capacity of C array 
         # copy all elements from currect array in to new larger array
@@ -63,20 +93,60 @@ class DynamicArray:
         self._array = double_capacity_array
 
 
+    def delete(self, index):
+        if self.is_empty():
+            raise IndexError("Can't delete item from empty array")
+        if index >= self.size() or index < 0:
+            raise IndexError("Index out of bound") 
+        
+        # when deleting an item, if the size is 1/4 of capacity, resize to half
+        if self.size() <= (1 / 4 * self.capacity()):
+            self.resize(self.capacity() // 2)
+
+        # remove element at index position
+        self._array[index] = 0
+        # Shift any element at the right of self._array[index] one position to the left
+        for i in range(index, self.size() - 1):
+            self._array[i] = self._array[i + 1]
+        
+        self._size -= 1
+    
+
+    def remove(self, item):
+        # for i in range(self.size(), -1, -1):
+        #     if self._array[i]  == item:
+        #         self.delete(i)
+        i = 0
+        # [ 7, 7, 7]
+        while i < self.size():
+            if self._array[i] == item:
+                # don't increment i, recheck this index
+                self.delete(i)
+            else:
+                i += 1
+
+
+    def to_list(self):
+        """Return the current elements as a regular Python list."""
+        return [self._array[i] for i in range(self._size)]
+
+
     def __str__(self):
-        list_repr = '['
-        for i in range(self.capacity()):
+        list_repr = '[ '
+        for i in range(self.size()):
             list_repr += str(self._array[i])
             list_repr += ' ' 
         list_repr += ']'
         return list_repr
     
 
-a = DynamicArray()
-for i in range(5000):
-    a.push(i+1)
+b = DynamicArray()
+for _ in range(3):
+    b.push(7)
+b.push(1)
+b.push(2)
+b.push(3)
+b.remove(7)
+b.remove(3)
+print(b.to_list(), b.size())
 
-print(a.capacity())
-
-for j in range(2953):
-    a.pop()
--- test_dynamic_array.py
+++ test_dynamic_array.py
@@ -1,51 +1,192 @@
 import unittest
-from dynamic_array import DynamicArray  # replace with your actual file name
+from dynamic_array import DynamicArray
 
-class TestDynamicArray(unittest.TestCase):
 
+# ------------------------ #
+#  BASIC CONSTRUCTOR TESTS #
+# ------------------------ #
+
+class TestDynamicArrayInit(unittest.TestCase):
+    def test_initial_capacity_and_size(self):
+        arr = DynamicArray()
+        self.assertEqual(arr.size(), 0)
+        self.assertGreaterEqual(arr.capacity(), 16)
+        self.assertTrue(arr.is_empty())
+
+    def test_custom_capacity(self):
+        arr = DynamicArray(8)
+        self.assertEqual(arr.capacity(), 8)
+        self.assertEqual(arr.size(), 0)
+
+
+# ------------------------ #
+#  PUSH / POP FUNCTIONALITY #
+# ------------------------ #
+
+class TestDynamicArrayPushPop(unittest.TestCase):
     def setUp(self):
         self.arr = DynamicArray()
 
-    def test_initial_state(self):
-        self.assertEqual(self.arr.size(), 0)
-        self.assertGreaterEqual(self.arr.capacity(), 16)
-        self.assertTrue(self.arr.is_empty())
-
-    def test_push_and_at(self):
+    def test_push_adds_items_correctly(self):
         for i in range(5):
             self.arr.push(i)
+        self.assertEqual(self.arr.to_list(), [0, 1, 2, 3, 4])
         self.assertEqual(self.arr.size(), 5)
-        self.assertFalse(self.arr.is_empty())
-        for i in range(5):
-            self.assertEqual(self.arr.at(i), i)
 
-    def test_push_triggers_resize(self):
-        initial_capacity = self.arr.capacity()
-        for i in range(initial_capacity + 1):
+    def test_push_triggers_resize_up(self):
+        cap = self.arr.capacity()
+        for i in range(cap + 1):
             self.arr.push(i)
-        self.assertGreater(self.arr.capacity(), initial_capacity)
+        self.assertGreater(self.arr.capacity(), cap)
 
-    def test_pop(self):
-        for i in range(5):
+    def test_pop_removes_last_element(self):
+        for i in range(4):
             self.arr.push(i)
         last = self.arr.pop()
-        self.assertEqual(last, 4)
-        self.assertEqual(self.arr.size(), 4)
+        self.assertEqual(last, 3)
+        self.assertEqual(self.arr.size(), 3)
+        self.assertEqual(self.arr.to_list(), [0, 1, 2])
 
-
-    def test_at_out_of_bounds(self):
+    def test_pop_from_empty_raises(self):
         with self.assertRaises(IndexError):
-            self.arr.at(0)
-        self.arr.push(1)
-        with self.assertRaises(IndexError):
-            self.arr.at(2)
+            self.arr.pop()
 
-    def test_is_empty_behavior(self):
-        self.assertTrue(self.arr.is_empty())
-        self.arr.push(10)
-        self.assertFalse(self.arr.is_empty())
-        self.arr.pop()
-        self.assertTrue(self.arr.is_empty())
+
+# ------------------------ #
+#  INDEX ACCESS (at) TESTS #
+# ------------------------ #
+
+class TestDynamicArrayAccess(unittest.TestCase):
+    def setUp(self):
+        self.arr = DynamicArray()
+        for i in range(5):
+            self.arr.push(i)
+
+    def test_valid_access(self):
+        self.assertEqual(self.arr.at(0), 0)
+        self.assertEqual(self.arr.at(4), 4)
+
+    def test_out_of_bounds_access(self):
+        with self.assertRaises(IndexError):
+            self.arr.at(10)
+        with self.assertRaises(IndexError):
+            self.arr.at(-1)
+
+
+# ------------------------ #
+#   INSERT / PREPEND TESTS #
+# ------------------------ #
+
+class TestDynamicArrayInsert(unittest.TestCase):
+    def setUp(self):
+        self.a = DynamicArray()
+        for i in [1, 2, 3, 4, 5]:
+            self.a.push(i)
+
+    def test_insert_at_start(self):
+        self.a.insert(0, 99)
+        self.assertEqual(self.a.to_list(), [99, 1, 2, 3, 4, 5])
+
+    def test_insert_in_middle(self):
+        self.a.insert(2, 77)
+        self.assertEqual(self.a.to_list(), [1, 2, 77, 3, 4, 5])
+
+    def test_insert_at_end(self):
+        self.a.insert(self.a.size(), 55)
+        self.assertEqual(self.a.to_list(), [1, 2, 3, 4, 5, 55])
+
+    def test_insert_negative_index_raises(self):
+        with self.assertRaises(IndexError):
+            self.a.insert(-2, 10)
+
+    def test_insert_beyond_end_raises(self):
+        with self.assertRaises(IndexError):
+            self.a.insert(self.a.size() + 1, 10)
+
+
+# ------------------------ #
+#        DELETE TESTS      #
+# ------------------------ #
+
+class TestDynamicArrayDelete(unittest.TestCase):
+    def setUp(self):
+        self.a = DynamicArray()
+        for i in range(6):
+            self.a.push(i)
+
+    def test_delete_first(self):
+        self.a.delete(0)
+        self.assertEqual(self.a.to_list(), [1, 2, 3, 4, 5])
+
+    def test_delete_middle(self):
+        self.a.delete(3)
+        self.assertEqual(self.a.to_list(), [0, 1, 2, 4, 5])
+
+    def test_delete_last(self):
+        self.a.delete(self.a.size() - 1)
+        self.assertEqual(self.a.to_list(), [0, 1, 2, 3, 4])
+
+    def test_delete_out_of_bounds(self):
+        with self.assertRaises(IndexError):
+            self.a.delete(999)
+
+    def test_delete_on_empty_raises(self):
+        b = DynamicArray()
+        with self.assertRaises(IndexError):
+            b.delete(0)
+
+    def test_delete_triggers_resize_down(self):
+        b = DynamicArray(8)
+        for i in range(8):
+            b.push(i)
+        for _ in range(7):
+            b.delete(0)
+        self.assertEqual(b.capacity(), 4)
+
+
+# ------------------------ #
+#         REMOVE TESTS     #
+# ------------------------ #
+
+class TestDynamicArrayRemove(unittest.TestCase):
+    def setUp(self):
+        self.a = DynamicArray()
+        for i in [1, 2, 3, 2, 4, 2, 5]:
+            self.a.push(i)
+
+    def test_remove_single_occurrence(self):
+        self.a.remove(3)
+        self.assertEqual(self.a.to_list(), [1, 2, 2, 4, 2, 5])
+
+    def test_remove_multiple_occurrences(self):
+        self.a.remove(2)
+        self.assertEqual(self.a.to_list(), [1, 3, 4, 5])
+
+    def test_remove_nonexistent_item(self):
+        before = self.a.to_list()
+        self.a.remove(99)
+        self.assertEqual(self.a.to_list(), before)
+
+    def test_remove_first_item(self):
+        self.a.remove(1)
+        self.assertEqual(self.a.to_list(), [2, 3, 2, 4, 2, 5])
+
+    def test_remove_last_item(self):
+        self.a.remove(5)
+        self.assertEqual(self.a.to_list(), [1, 2, 3, 2, 4, 2])
+
+    def test_remove_all_items(self):
+        b = DynamicArray()
+        for _ in range(3):
+            b.push(7)
+        b.remove(7)
+        self.assertTrue(b.is_empty())
+
+    def test_remove_from_empty(self):
+        b = DynamicArray()
+        b.remove(10)
+        self.assertTrue(b.is_empty())
+
 
 if __name__ == '__main__':
     unittest.main()
