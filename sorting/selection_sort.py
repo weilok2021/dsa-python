1aa6f7b3a52f54c37dd500b3607e9968d693d48a
weilok2021 <weilok629@gmail.com>
2025-10-20 16:55:38 +0800

    Enhanced selection sort implementation to using only one list

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,22 +1,24 @@
-def find_smallest(arr):
-    smallest = arr[0]
-    smallest_i = 0
-    for i in range(1, len(arr)):
-        if arr[i] < smallest:
-            smallest = arr[i]
-            smallest_i = i
-    return smallest_i
+# What this algorithm does?
 
+# Initialize smallest element to arr[i] for every ith iteration
+# Find the smallest element in the inner for loop
+# Then swap this smallest element with current ith element 
 
 def selection_sort(arr):
-    new_arr = []
-    while arr:
-        smallest_i = find_smallest(arr)
-        new_arr.append(arr[smallest_i])
-        arr.pop(smallest_i)
-    return new_arr
+    for i in range(len(arr)):
+        smallest_elem = arr[i]
+        smallest_index = i
+        for j in range(i, len(arr)):
+            if arr[j] < smallest_elem:
+                smallest_elem = arr[j]
+                smallest_index = j
+        # swap the current element with the smallest element in this iteration
+        temp = arr[i] 
+        arr[i] = smallest_elem 
+        arr[smallest_index] = temp 
+    return arr
 
 
-print(selection_sort([3,1,2]))
+print(selection_sort([]))
 
     
